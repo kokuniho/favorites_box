eb461144eaa944507fe4ea5f0bf8ab8a358d05f2
kokuniho <ishikuni.9240@gmail.com>
2022-03-07 05:23:09 +0000

    edit/layout

--- app/controllers/end_users_controller.rb
+++ app/controllers/end_users_controller.rb
@@ -1,4 +1,7 @@
 class EndUsersController < ApplicationController
+   before_action :authenticate_end_user!
+   before_action :ensure_guest_end_user, only: [:edit]
+   
   def index
     @end_users =EndUser.page(params[:page])
 
@@ -6,16 +9,48 @@ class EndUsersController < ApplicationController
 
   def show
     @end_user = EndUser.find(params[:id])
-    @items= Item.find_by(id: @end_user.item_ids)
-    @items = params[:tag_id].present? ? Tag.find(params[:tag_id]).items : @end_user.items.all
+    # @items= Item.find_by(id: @end_user.item_ids)
+    # @items=Item.where(end_user_id:@end_user.id)
+    @item = params[:tag_id].present? ? Tag.find(params[:tag_id]).items : @end_user.items.all
     @items = @end_user.items.page(params[:page])
+  end
+
+  def edit
+    @end_user = EndUser.find(params[:id])
+     if @end_user == current_end_user
+      render "edit"
+     else
+      redirect_to edit_end_user_path(current_end_user)
+     end
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to end_user_path(@end_user.id)
+      flash[:notice] ="You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
 
   private
 
   def end_user_params
-    params.require(:end_user).permit(:name, :introduction, :profile_image)
+    params.require(:end_user).permit(:name, :introduction, :profile_image, tag_ids: [])
+  end
+  
+  def correct_end_user
+    @end_user = EndUser.find(params[:id])
+     redirect_to(end_user_session_path(end_user_id)) unless @end_user == current_end_user
+  end
+  
+  def ensure_guest_end_user
+    @end_user = EndUser.find(params[:id])
+    if @end_user.name == "guestuser"
+      redirect_to end_user_path(current_end_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
   end
 
 end
