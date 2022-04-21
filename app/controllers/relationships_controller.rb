d92f9aade0c36cd95379da90c4f51049fb916420
kokuniho <ishikuni.9240@gmail.com>
2022-04-21 08:27:58 +0000

    addname

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -14,13 +14,13 @@ class RelationshipsController < ApplicationController
     end
     #フォロー一覧
     def followings
-      end_user = EndUser.find(params[:end_user_id])
-      @end_users = end_user.followings
+      @end_user = EndUser.find(params[:end_user_id])
+      @end_users = @end_user.followings
     end
     #フォロワー一覧
     def followers
-      end_user = EndUser.find(params[:end_user_id])
-      @end_users = end_user.followers
+      @end_user = EndUser.find(params[:end_user_id])
+      @end_users = @end_user.followers
     end
 end
 
